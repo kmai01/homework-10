4b2221badd700f095c00b1cdf2141672aefc4be2
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-03-07 02:35:28 -0600

    add prompt

--- app.js
+++ app.js
@@ -2,5 +2,54 @@ const Employee = require("./lib/Employee");
 
 const employee = new Employee()
 
-// Get Name 
-employee.getName();
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your manager's name?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is your manager's id?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your manager's email?"
+    },
+    {
+      type: "input",
+      name: "officenumber",
+      message: "What is your manager's office number?"
+    },
+    {
+      type: "input",
+      name: "role",
+      message: "Which type of team member would you like to add?"
+    },
+    
+]);
+}
+
+
+
+promptUser()
+  .then(function(answers) {
+    const html = generateHTML(answers);
+
+    // return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
